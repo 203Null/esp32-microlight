b8fc79a011590f9b27f14d29e29fb40f47280d51
203Null <cainengzhuo@gmail.com>
2021-09-30 02:37:34 -0400

    Working release. 9/29 live stream

--- main/main.c
+++ main/main.c
@@ -2,6 +2,7 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/timers.h"
 
 #include "sdkconfig.h"
 
@@ -16,7 +17,11 @@
 #include "led_strip.h"
 #include "tusb.h"
 
+#include "data.h"
+
 static const char *TAG = "Microlight";
+
+#define BRIGHTNESS 32
 
 #define RMT_TX_CHANNEL RMT_CHANNEL_0
 
@@ -49,7 +54,7 @@ void delay(uint32_t ms)
 
 void setup_led(void) //配置WS2812驱动硬件（RMT)
 {
-    rmt_config_t config = RMT_DEFAULT_CONFIG_TX(48, RMT_TX_CHANNEL);
+    rmt_config_t config = RMT_DEFAULT_CONFIG_TX(14, RMT_TX_CHANNEL);
     // set counter clock to 40MHz
         // set counter clock to 40MHz
     config.clk_div = 2;
@@ -58,7 +63,7 @@ void setup_led(void) //配置WS2812驱动硬件（RMT)
     ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
 
     // install ws2812 driver
-    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(1, (led_strip_dev_t)config.channel);
+    led_strip_config_t strip_config = LED_STRIP_DEFAULT_CONFIG(64, (led_strip_dev_t)config.channel);
     strip = led_strip_new_rmt_ws2812(&strip_config);
     if (!strip) {
         ESP_LOGE(TAG, "install WS2812 driver failed");
@@ -153,10 +158,26 @@ void midi_task(void* param)
   while ( tud_midi_available() ) 
   {
     tud_midi_packet_read(packet); 
-    set_color(0, colors[color_index % 7], 32);
-    update_strip();
-    color_index++;
-    tud_midi_packet_write(packet); 
+    switch (packet[0]) 
+    {
+      case 0x08: //CIN_NOTE_OFF
+      case 0x09: //CIN_NOTE_ON
+      {
+        uint8_t channel = packet[1] & 0x0F;
+        uint8_t note = packet[2];
+        uint8_t velocity = packet[3];
+
+        if (note > 35 && note < 100)
+        {   
+          uint8_t index = user1_keymap_optimized[note - 36];
+          set_color(index, palette[channel % 2][velocity], BRIGHTNESS);
+        }
+      }
+    }
+    // set_color(0, colors[color_index % 7], 32);
+    // update_strip();
+    // color_index++;
+    // tud_midi_packet_write(packet); 
   }
 }
 
@@ -165,61 +186,9 @@ static inline uint32_t board_millis(void)
   return ( ( ((uint64_t) xTaskGetTickCount()) * 1000) / configTICK_RATE_HZ );
 }
 
-
-// // Variable that holds the current position in the sequence.
-// uint32_t note_pos = 0;
-
-// // Store example melody as an array of note values
-// uint8_t note_sequence[] =
-// {
-//   74,78,81,86,90,93,98,102,57,61,66,69,73,78,81,85,88,92,97,100,97,92,88,85,81,78,
-//   74,69,66,62,57,62,66,69,74,78,81,86,90,93,97,102,97,93,90,85,81,78,73,68,64,61,
-//   56,61,64,68,74,78,81,86,90,93,98,102
-// };
-
-// void midi_task(void* param)
-// {
-//   (void) param;
-
-//   static uint32_t start_ms = 0;
-
-//   uint8_t const cable_num = 0; // MIDI jack associated with USB endpoint
-//   uint8_t const channel   = 0; // 0 for channel 1
-
-//   // The MIDI interface always creates input and output port/jack descriptors
-//   // regardless of these being used or not. Therefore incoming traffic should be read
-//   // (possibly just discarded) to avoid the sender blocking in IO
-//   uint8_t packet[4];
-//   while ( tud_midi_available() ) tud_midi_packet_read(packet);
-
-//   // send note periodically
-//   if (board_millis() - start_ms < 286) return; // not enough time
-//   start_ms += 286;
-
-//   // Previous positions in the note sequence.
-//   int previous = note_pos - 1;
-
-//   // If we currently are at position 0, set the
-//   // previous position to the last note in the sequence.
-//   if (previous < 0) previous = sizeof(note_sequence) - 1;
-
-//   // Send Note On for current position at full velocity (127) on channel 1.
-//   uint8_t note_on[3] = { 0x90 | channel, note_sequence[note_pos], 127 };
-//   tud_midi_stream_write(cable_num, note_on, 3);
-
-//   // Send Note Off for previous note.
-//   uint8_t note_off[3] = { 0x80 | channel, note_sequence[previous], 0};
-//   tud_midi_stream_write(cable_num, note_off, 3);
-
-//   set_color(0, colors[note_pos % 7], 32);
-//   update_strip();
-
-//   // Increment position
-//   note_pos++;
-
-//   // If we are at the end of the sequence, start over.
-//   if (note_pos >= sizeof(note_sequence)) note_pos = 0;
-// }
+// static timer
+StaticTimer_t led_tmdef;
+TimerHandle_t led_tm;
 
 void app_main(void)
 {
@@ -227,8 +196,7 @@ void app_main(void)
     setup_led();
     tusb_init();
 
-    set_color(0, 0x00FFFF, 128);
-    update_strip();
+    // set_color(0, 0x00FFFF, 128);
 
     // while (1)
     // {
@@ -240,13 +208,17 @@ void app_main(void)
     // usb_device_task(0);
     // Create a task for tinyusb device stack
     (void) xTaskCreateStatic( usb_device_task, "usbd", USBD_STACK_SIZE, NULL, configMAX_PRIORITIES-1, usb_device_stack, &usb_device_taskdef);
-
+    
     // Create LED task
     // led_task(0);
     // (void) xTaskCreate( led_task, "led", LED_STACK_SZIE, NULL, configMAX_PRIORITIES-2, &led_taskdef);
 
     // Create Midi task
     // (void) xTaskCreateStatic( midi_task, "midi", MIDI_STACK_SIZE, NULL, configMAX_PRIORITIES-2, midi_stack, &midi_taskdef);
+
+    // soft timer for led update
+    led_tm = xTimerCreateStatic(NULL, pdMS_TO_TICKS(10), true, NULL, update_strip, &led_tmdef);
+    xTimerStart(led_tm, 0);
     while (1)
     {
       // tinyusb device task
